22ac4b7e30fa438ae82ab891130ab8f548a06ed7
David Collado <bitomule@gmail.com>
2026-05-03 09:02:03 +0200

    Prepare v0.0.1 Homebrew release

--- packaging/homebrew/mav.rb
+++ packaging/homebrew/mav.rb
@@ -1,15 +1,15 @@
 class Mav < Formula
   desc "Mobile Agent Verifier for iOS Bazel apps"
   homepage "https://github.com/bitomule/mav"
-  version "0.1.0"
+  version "0.0.1"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bitomule/mav/releases/download/v#{version}/mav-darwin-arm64"
-      sha256 "REPLACE_WITH_ARM64_SHA256"
+      sha256 "c5aa187a6671095cf6bb13fd0c2757c3f9c2742af51bc830693f3d6269464055"
     else
       url "https://github.com/bitomule/mav/releases/download/v#{version}/mav-darwin-amd64"
-      sha256 "REPLACE_WITH_AMD64_SHA256"
+      sha256 "1dfda80d01dd288f031723db163a68ff56dcadee3ecbf150b243cc43d2e256e5"
     end
   end
 
