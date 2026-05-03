a146ed7b816993359130e986433eaf56d5b9b67a
David Collado <bitomule@gmail.com>
2026-05-03 09:05:55 +0200

    Update Homebrew checksums for v0.0.1

--- packaging/homebrew/mav.rb
+++ packaging/homebrew/mav.rb
@@ -6,15 +6,16 @@ class Mav < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bitomule/mav/releases/download/v#{version}/mav-darwin-arm64"
-      sha256 "c5aa187a6671095cf6bb13fd0c2757c3f9c2742af51bc830693f3d6269464055"
+      sha256 "d6881612e0c3e3da51ca39165d5c4cbfaefafaac051b3bcb2e3eb1121121a36e"
     else
       url "https://github.com/bitomule/mav/releases/download/v#{version}/mav-darwin-amd64"
-      sha256 "1dfda80d01dd288f031723db163a68ff56dcadee3ecbf150b243cc43d2e256e5"
+      sha256 "ff531122b41991823eddc602f97664c54cd6cf7ae556ab39abee68096d81d33a"
     end
   end
 
   def install
     bin.install cached_download => "mav"
+    chmod 0755, bin/"mav"
   end
 
   test do
